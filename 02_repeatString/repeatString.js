5e5d0f2f9f1bdefe2e28d461918d58b03f12e04c
Andrei <andreireinaldofarias@gmail.com>
2021-08-27 10:20:37 -0300

    Maybe halfway there?

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let testString = '';
 
+    if (num < 0){
+        return ('ERROR');
+    }
+    for(i = 1; i <= num; i++) {
+        testString += string;
+    }
+    return testString;
 };
+
 
 module.exports = repeatString;
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
 const reverseString = function(str) {
+    let testString = "";
 
+    for(i = str.length - 1; i >= 0; i--) {
+        testString += str[i];
+    }
+    return testString;
 };
 
 module.exports = reverseString;
