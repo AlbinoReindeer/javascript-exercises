90577522d548c1766138a35a4e1c8b914b9e588a
Andrei <andreireinaldofarias@gmail.com>
2021-08-29 14:36:30 -0300

    7 down, n more to go.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(num, secondNum) {
+    let sum = 0;
+    if (!Number.isInteger(arguments[0]) || !Number.isInteger(arguments[1])) return "ERROR";
+    if (arguments[0] < 0 || arguments[1] < 0) return "ERROR";
+    if (arguments[0] > arguments[1]) {
+        for (i = arguments[1]; i <= arguments[0]; i++) {
+            sum += i;
+        }
+    } else {
+        for (i = arguments[0]; i <= arguments[1]; i++) {
+            sum += i;
+        }
+    }
 
+    return sum;
 };
 
 module.exports = sumAll;
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
 const ftoc = function() {
-
+  let celsius = (arguments[0] - 32) * 5/9;
+  celsius = Math.round(celsius * 10) / 10;
+  return celsius;
 };
 
 const ctof = function() {
-
+  let fahrenheit = arguments[0] * 9/5 + 32;
+  fahrenheit = Math.round(fahrenheit * 10) / 10;
+  return fahrenheit;
 };
 
 module.exports = {
